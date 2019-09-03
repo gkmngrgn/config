cb401db1d1618118ac400067fd984f53347725f3
Gökmen Görgen <gkmngrgn@gmail.com>
2019-09-03 18:34:32 +0200

    fix indentation.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,14 +17,14 @@
 (set-selection-coding-system 'utf-8)
 
 (setq-default
- indent-tabs-mode nil
- truncate-lines t)
+    indent-tabs-mode nil
+    truncate-lines t)
 
 (setq
- inhibit-splash-screen t
- scroll-conservatively 10000
- scroll-step 1
- visible-bell 1)
+    inhibit-splash-screen t
+    scroll-conservatively 10000
+    scroll-step 1
+    visible-bell 1)
 
 (menu-bar-mode -1)
 (line-number-mode t)
@@ -42,15 +42,15 @@
 
 ;; Place all backup files in one directory
 (setq
- backup-directory-alist `((".*" . ,temporary-file-directory))
- auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
- default-directory (concat (getenv "HOME") "/Workspace"))
+    backup-directory-alist `((".*" . ,temporary-file-directory))
+    auto-save-file-name-transforms `((".*" ,temporary-file-directory t))
+    default-directory (concat (getenv "HOME") "/Workspace"))
 
 ;; Custom variables
 (defvar custom-file-path "~/.emacs.d/custom.el")
 (setq custom-file custom-file-path)
 (when (file-exists-p custom-file-path)
-  (load custom-file))
+    (load custom-file))
 
 ;; Packages
 (require 'package)
@@ -61,147 +61,148 @@
                             ("melpa-stable" . "https://stable.melpa.org/packages/")))
 
 (when (not package-archive-contents)
-  (package-refresh-contents))
+    (package-refresh-contents))
 
 (defvar my-packages
-  '(
-    ;; file modes
-    dockerfile-mode
-    dart-mode
-    go-mode
-    js2-mode
-    markdown-mode
-    rust-mode
-    scss-mode
-    web-mode
-    yaml-mode
+    '(
+         ;; file modes
+         dockerfile-mode
+         dart-mode
+         go-mode
+         js2-mode
+         markdown-mode
+         rust-mode
+         scss-mode
+         web-mode
+         yaml-mode
 
-    ;; common
-    editorconfig
-    magit
-    use-package
-    color-theme-sanityinc-tomorrow
+         ;; common
+         editorconfig
+         magit
+         use-package
+         color-theme-sanityinc-tomorrow
 
-    ;; navigation plugins
-    flx-ido
-    helm
-    helm-ag
-    helm-ls-git
+         ;; navigation plugins
+         flx-ido
+         helm
+         helm-ag
+         helm-ls-git
 
-    ;; language server protocol
-    company
-    company-lsp
-    flycheck
-    lsp-mode
-    lsp-ui)
-  "A list of packages to ensure are installed at launch.")
+         ;; language server protocol
+         company
+         company-lsp
+         flycheck
+         lsp-mode
+         lsp-ui)
+    "A list of packages to ensure are installed at launch.")
 
 (dolist (p my-packages)
-  (when (not (package-installed-p p)) (package-install p)))
+    (when (not (package-installed-p p)) (package-install p)))
 
 (defun kill-other-buffers ()
-  "Kill all other buffers."
-  (interactive)
-  (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))
-  (message "All other buffers are killed.."))
+    "Kill all other buffers."
+    (interactive)
+    (mapc 'kill-buffer (delq (current-buffer) (buffer-list)))
+    (message "All other buffers are killed.."))
 
 (global-set-key (kbd "C-c k") 'kill-other-buffers)
 
 ;; GUI settings
 (if (display-graphic-p)
     (progn
-      (scroll-bar-mode -1)
-      (tool-bar-mode -1)
-      (global-hl-line-mode)
-      (load-theme 'sanityinc-tomorrow-night t)
-      (if (eq system-type 'windows-nt)
-          (set-face-attribute 'default nil
-                              :font "Consolas"
-                              :height 90
-                              :weight 'normal
-                              :width 'normal))))
+        (scroll-bar-mode -1)
+        (tool-bar-mode -1)
+        (global-hl-line-mode)
+        (load-theme 'sanityinc-tomorrow-night t)
+        (if (eq system-type 'windows-nt)
+            (set-face-attribute 'default nil
+                :font "Consolas"
+                :height 90
+                :weight 'normal
+                :width 'normal))))
 
 ;; Package Configurations
 (use-package flx-ido
-  :config
-  (setq
-   ido-enable-flex-matching t
-   ido-use-faces nil)
-  (ido-mode 1) ; TODO: do we need ido-mode?
-  (ido-everywhere 1)
-  (flx-ido-mode 1))
+    :config
+    (setq
+        ido-enable-flex-matching t
+        ido-use-faces nil)
+    (ido-mode 1) ; TODO: do we need ido-mode?
+    (ido-everywhere 1)
+    (flx-ido-mode 1))
 
 (use-package web-mode
-  :mode ("\\.html$" . web-mode)
-  :config
-  (setq web-mode-markup-indent-offset 2
+    :mode ("\\.html$" . web-mode)
+    :config
+    (setq web-mode-markup-indent-offset 2
         web-mode-css-indent-offset 2
         web-mode-code-indent-offset 2
         web-mode-engines-alist '(("django" . "\\.html$"))
         web-mode-enable-auto-pairing nil))
 
 (use-package lsp-mode
-  :commands lsp
-  :init
-  (setq lsp-prefer-flymake nil) ; I use flycheck instead.
-  (add-hook 'dart-mode-hook #'lsp)
-  (add-hook 'go-mode-hook #'lsp)
-  (add-hook 'python-mode-hook #'lsp)
-  :config
-  (use-package lsp-ui
-    :commands lsp-ui-mode)
-  (use-package company-lsp
-    :commands company-lsp))
+    :commands lsp
+    :init
+    (setq lsp-prefer-flymake nil) ; I use flycheck instead.
+    (add-hook 'dart-mode-hook #'lsp)
+    (add-hook 'go-mode-hook #'lsp)
+    (add-hook 'python-mode-hook #'lsp)
+
+    :config
+    (use-package lsp-ui
+        :commands lsp-ui-mode)
+    (use-package company-lsp
+        :commands company-lsp))
 
 (use-package company
-  :bind ("C-c TAB" . company-complete)
-  :config
-  (global-company-mode))
+    :bind ("C-c TAB" . company-complete)
+    :config
+    (global-company-mode))
 
 (use-package flycheck
-  :config
-  (global-flycheck-mode))
+    :config
+    (global-flycheck-mode))
 
 (use-package js2-mode
-  :mode ("\\.js$" . js2-mode)
-  :config
-  (add-hook 'js2-mode-hook
-            '(lambda ()
-               (setq
-                js2-pretty-multiline-decl-indentation-p t
-                js2-consistent-level-indent-inner-bracket-p t
-                js2-basic-offset 2))))
+    :mode ("\\.js$" . js2-mode)
+    :config
+    (add-hook 'js2-mode-hook
+        '(lambda ()
+             (setq
+                 js2-pretty-multiline-decl-indentation-p t
+                 js2-consistent-level-indent-inner-bracket-p t
+                 js2-basic-offset 2))))
 
 (use-package css-mode
-  :config
-  (setq css-indent-offset 2))
+    :config
+    (setq css-indent-offset 2))
 
 (use-package scss-mode
-  :mode (("\\.scss$" . scss-mode)
-         ("\\.sass$" . scss-mode)))
+    :mode (("\\.scss$" . scss-mode)
+              ("\\.sass$" . scss-mode)))
 
 (use-package helm-mode
-  :config
-  (global-set-key (kbd "M-x") 'helm-M-x)
-  (global-set-key (kbd "C-c f") 'helm-find-files)
-  (global-set-key (kbd "C-c s") 'helm-ag-project-root)
-  (global-set-key (kbd "C-x C-d") 'helm-browse-project))
+    :config
+    (global-set-key (kbd "M-x") 'helm-M-x)
+    (global-set-key (kbd "C-c f") 'helm-find-files)
+    (global-set-key (kbd "C-c s") 'helm-ag-project-root)
+    (global-set-key (kbd "C-x C-d") 'helm-browse-project))
 
 (use-package markdown-mode
-  :defer t
-  :mode (("\\.md?\\'" . markdown-mode))
-  :config
-  (set-face-attribute 'markdown-code-face nil
-                      :inherit nil
-                      :foreground "dim gray")
-  (add-hook 'markdown-mode-hook 'visual-line-mode))
+    :defer t
+    :mode (("\\.md?\\'" . markdown-mode))
+    :config
+    (set-face-attribute 'markdown-code-face nil
+        :inherit nil
+        :foreground "dim gray")
+    (add-hook 'markdown-mode-hook 'visual-line-mode))
 
 (use-package editorconfig
-  :config
-  (editorconfig-mode 1))
+    :config
+    (editorconfig-mode 1))
 
 (use-package magit
-  :bind ("C-c g" . magit-status))
+    :bind ("C-c g" . magit-status))
 
 ;;; Local Variables:
 ;; coding: utf-8
