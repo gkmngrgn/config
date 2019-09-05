5c6057d947182e514614eda3225dddb67b88bf84
Gökmen Görgen <gkmngrgn@gmail.com>
2019-09-05 09:31:40 +0200

    fix copy paste problem in windows.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -14,7 +14,7 @@
 (prefer-coding-system 'utf-8)
 (set-default-coding-systems 'utf-8)
 (set-language-environment 'utf-8)
-(set-selection-coding-system 'utf-8)
+;; (set-selection-coding-system 'utf-8)
 
 (setq-default
     indent-tabs-mode nil
