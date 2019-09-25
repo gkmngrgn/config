6be932c865282e25064552974591626c065c7763
Gökmen Görgen <gkmngrgn@gmail.com>
2019-09-25 21:01:43 +0200

    unnecessary configs.

--- powershell_default_profile.ps1
+++
@@ -1,7 +0,0 @@
-Import-Module posh-git
-
-function prompt {
-    $prompt = & $GitPromptScriptBlock
-    $prompt += Write-Prompt "`r`n$ " -ForegroundColor Green
-    if ($prompt) {$prompt} else {" "}
-}
