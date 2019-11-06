de788664dd495bf5368c7942aa4e1596b8845a88
Gökmen Görgen <gkmngrgn@gmail.com>
2019-11-06 23:12:47 +0100

    fix telephone-line.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -246,12 +246,11 @@
     :group 'telephone-line)
 
   (setq telephone-line-lhs
-        '((evil . (telephone-line-evil-tag-segment))
-          (gray . (telephone-line-vc-segment
+        '((red  . (telephone-line-vc-segment
                    telephone-line-erc-modified-channels-segment
                    telephone-line-process-segment))
-          (nil  . (telephone-line-minor-mode-segment
-                   telephone-line-buffer-segment))))
+          (gray . (telephone-line-minor-mode-segment))
+          (nil  . (telephone-line-buffer-segment))))
 
   (setq telephone-line-rhs
         '((nil  . (telephone-line-misc-info-segment))
