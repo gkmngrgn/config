9802722129ecb628590148a13bdc650406a49595
Gökmen Görgen <goekmen.goergen@mimi.io>
2022-07-09 18:09:47 +0200

    add linuxbrew config.

--- home/.goedev-env.sh
+++ home/.goedev-env.sh
@@ -30,3 +30,9 @@ export PATH="$DENO_INSTALL/bin:$PATH"
 # NODE
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# LINUX BREW
+LINUXBREW=/home/linuxbrew/.linuxbrew/bin/brew
+if test -f "$LINUXBREW"; then
+    eval "$($LINUXBREW shellenv)"
+fi
