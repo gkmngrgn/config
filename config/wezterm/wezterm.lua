8f6b5b4cfe0e0735b1024ab3fb633f4bcc0c38c6
Gökmen Görgen <goekmen.goergen@mimi.io>
2022-12-05 22:40:20 +0100

    tab bar configuration.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,6 +1,9 @@
 local wezterm = require 'wezterm'
 
 return {
+   tab_bar_at_bottom = true,
+   tab_max_width = 30,
+   use_fancy_tab_bar = false,
    font_size = 15,
    window_padding = {
       top = 0,
