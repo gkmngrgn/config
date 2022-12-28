8219841b613a214d5106b47ccbf966aad8fa7d70
Gökmen Görgen <gkmngrgn@gmail.com>
2022-12-28 04:26:03 +0300

    update wezterm config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,10 +1,13 @@
 local wezterm = require 'wezterm'
 
 return {
+   initial_cols = 130,
+   initial_rows = 40,
    tab_bar_at_bottom = true,
    tab_max_width = 30,
    use_fancy_tab_bar = false,
    font_size = 15,
+   window_decorations = "TITLE",
    window_padding = {
       top = 0,
       right = 0,
