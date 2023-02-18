3e77bd2581e952772f631b14bb5c34489bf71346
Gökmen Görgen <gkmngrgn@gmail.com>
2023-02-18 13:34:40 +0300

    reduce rows of wezterm window.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
    initial_cols = 130,
-   initial_rows = 40,
+   initial_rows = 38,
    tab_bar_at_bottom = true,
    tab_max_width = 30,
    use_fancy_tab_bar = false,
