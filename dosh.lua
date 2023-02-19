3b5d0f598a05db5e0ec259899ef6c0ebedf4de41
Gökmen Görgen <gkmngrgn@gmail.com>
2023-02-20 00:00:26 +0300

    cmd.ls is valid anymore.

--- dosh.lua
+++ dosh.lua
@@ -4,31 +4,43 @@ cmd.add_task {
     name = "setup",
     description = "setup my operating system.",
     required_platforms = {"macos"},
+    command_options = {
+        setup_type = {
+            type = "string",
+            options = {"all", "emacs", "python", "shell", "tmux"},
+            default = "all"
+        }
+    },
     command = function(arg)
         arg = arg or "all"
         local shell_type = env.IS_ZSH and "zsh" or "bash"
 
-        -- check if all required packages are installed
-        cmd.brew_install({
-            "bat", "exa", "git-delta", "git-lfs", "htop", "nvm", "openssl",
-            "font-ibm-plex", "wezterm"
-        }, {taps = {"wez/wezterm"}})
+        if arg == "all" then
+            -- check if all required packages are installed
+            cmd.brew_install({
+                "bat", "exa", "git-delta", "git-lfs", "htop", "nvm", "openssl",
+                "font-ibm-plex", "wezterm"
+            }, {taps = {"wez/wezterm"}})
 
-        -- copy all configuration files
-        local config_dirs = {"alacritty", "kitty", "nano", "wezterm"}
-        for index = 1, #config_dirs do
-            cmd.copy("./" .. config_dirs[index] .. "/", config_dir)
+            -- copy all configuration files
+            local config_dirs = cmd.ls(".", {
+                include_files = false,
+                excludes = {".git", "archived", "home"}
+            }).response -- get rid of this `response`.
+            for index = 1, #config_dirs do
+                cmd.copy(config_dirs[index], config_dir)
+            end
+
+            cmd.copy("./home/*", "~")
         end
 
-        cmd.copy("./home/*", "~")
-
-        if arg == "tmux" or arg == "all" then
+        if cmd.any(options.setup_type, {"tmux", "all"}) then
             cmd.brew_install({"tmux"})
             cmd.clone("https://github.com/tmux-plugins/tpm",
                       {destination = "~/.tmux/plugins/tpm", fetch = true})
         end
 
-        if arg == "shell" or arg == "all" then
+        if cmd.any(options.setup_type, {"shell", "all"}) then
             if env.IS_ZSH then
                 if not cmd.exists("~/.oh-my-zsh") then
                     cmd.run_url(
@@ -37,12 +49,12 @@ cmd.add_task {
             end
         end
 
-        if arg == "python" or arg == "all" then
+        if cmd.any(options.setup_type, {"python", "all"}) then
             cmd.brew_install({"mambaforge"})
             cmd.run("mamba init " .. shell_type)
         end
 
-        if arg == "emacs" or arg == "all" then
+        if cmd.any(options.setup_type, {"emacs", "all"}) then
             cmd.run_url(
                 "https://raw.githubusercontent.com/gkmngrgn/emacs.d/main/dosh.lua",
                 {parameters = "install"})
