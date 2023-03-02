78caf19006ed9827a2efe33807c8d55765ddcd84
Gökmen Görgen <gkmngrgn@gmail.com>
2023-03-03 01:39:37 +0300

    add alias for edit.

--- home/.goedev-env.sh
+++ home/.goedev-env.sh
@@ -7,6 +7,8 @@ export EDITOR="emacsclient -t"
 export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 
+alias edit='$EDITOR'
+
 if command -v /opt/homebrew/bin/brew &> /dev/null
 then
    eval "$(/opt/homebrew/bin/brew shellenv)"
