93f80c5505929da766b823a686e5715aefbdbd1f
Gökmen Görgen <gkmngrgn@gmail.com>
2023-03-03 03:01:03 +0300

    fix edit alias.

--- home/.goedev-env.sh
+++ home/.goedev-env.sh
@@ -8,7 +8,7 @@ export LANG="en_US.UTF-8"
 export LC_ALL="en_US.UTF-8"
 export PATH="$HOME/.local/bin:$PATH"
 
-alias edit='$EDITOR'
+alias edit='eval "$EDITOR"'
 
 if command -v /opt/homebrew/bin/brew &> /dev/null
 then
