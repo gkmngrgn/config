750148c98c72deeeaa9995810131791cb71fc1d1
Gökmen Görgen <gkmngrgn@gmail.com>
2023-03-10 22:19:40 +0100

    update wezterm config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,24 +1,15 @@
 local wezterm = require 'wezterm'
 
 return {
-   initial_cols = 130,
-   initial_rows = 38,
-   tab_bar_at_bottom = true,
-   tab_max_width = 30,
-   use_fancy_tab_bar = false,
-   font_size = 15,
-   window_padding = {
-      top = 0,
-      right = 0,
-      bottom = 0,
-      left = 0,
-   },
-   audible_bell = 'Disabled',
-   ssh_domains = {
-      {
-         name = 'gerudo',
-         remote_address = 'gerudo',
-         username = 'goedev',
-      },
-   },
+    initial_cols = 128,
+    initial_rows = 32,
+    tab_max_width = 30,
+    use_fancy_tab_bar = false,
+    font_size = 15,
+    audible_bell = 'Disabled',
+    window_padding = {top = 0, left = 0, right = 0, bottom = 0},
+    window_decorations = "NONE",
+    ssh_domains = {
+        {name = 'gerudo', remote_address = 'gerudo', username = 'goedev'}
+    }
 }
