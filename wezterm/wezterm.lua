69e8f6c8eedd1a75f62eab9534a79e879b5dd867
Gökmen Görgen <gkmngrgn@gmail.com>
2023-04-11 21:12:06 +0200

    update wezterm config.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,14 +1,19 @@
 local wezterm = require 'wezterm'
 
 return {
-    initial_cols = 128,
-    initial_rows = 32,
+    font = wezterm.font {
+        family = 'IBM Plex Mono',
+        stretch = 'Expanded',
+        weight = 'Regular',
+    },
+    font_size = 15,
+    initial_cols = 160,
+    initial_rows = 42,
     tab_max_width = 30,
     use_fancy_tab_bar = false,
-    font_size = 15,
-    audible_bell = 'Disabled',
     window_padding = {top = 0, left = 0, right = 0, bottom = 0},
-    window_decorations = "TITLE",
+    audible_bell = 'Disabled',
+    window_decorations = 'TITLE',
     ssh_domains = {
         {name = 'gerudo', remote_address = 'gerudo', username = 'goedev'}
     }
