13e58af2bde0a6199d309cb5106a64abe9f3ec81
Gökmen Görgen <gkmngrgn@gmail.com>
2023-04-19 12:46:51 +0200

    light dark theme toggle for system preference.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,17 +1,33 @@
 local wezterm = require 'wezterm'
 
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function scheme_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return 'One Dark (Gogh)'
+  else
+    return 'One Light (Gogh)'
+  end
+end
+
 return {
-    font = wezterm.font {
-        family = 'Jetbrains Mono',
-        stretch = 'Expanded',
-        weight = 'Regular',
-    },
-    font_size = 15,
-    initial_cols = 160,
-    initial_rows = 42,
-    tab_max_width = 30,
-    use_fancy_tab_bar = false,
-    window_padding = {top = 0, left = 0, right = 0, bottom = 0},
-    audible_bell = 'Disabled',
-    window_decorations = 'TITLE',
+  color_scheme = scheme_for_appearance(get_appearance()),
+  font = wezterm.font {
+    family = 'Jetbrains Mono',
+    stretch = 'Expanded',
+    weight = 'Regular',
+  },
+  font_size = 15,
+  initial_cols = 160,
+  initial_rows = 42,
+  tab_max_width = 30,
+  use_fancy_tab_bar = false,
+  window_padding = { top = 0, left = 0, right = 0, bottom = 0 },
+  audible_bell = 'Disabled',
+  window_decorations = 'TITLE|RESIZE',
 }
