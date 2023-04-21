fa4850523847659686a6740899ba309050308b6b
Gökmen Görgen <gkmngrgn@gmail.com>
2023-04-21 03:11:09 +0300

    disable light theme in wezterm.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,22 +1,6 @@
 local wezterm = require 'wezterm'
 
-function get_appearance()
-  if wezterm.gui then
-    return wezterm.gui.get_appearance()
-  end
-  return 'Dark'
-end
-
-function scheme_for_appearance(appearance)
-  if appearance:find 'Dark' then
-    return 'One Dark (Gogh)'
-  else
-    return 'One Light (Gogh)'
-  end
-end
-
 return {
-  color_scheme = scheme_for_appearance(get_appearance()),
   font = wezterm.font {
     family = 'Jetbrains Mono',
     stretch = 'Expanded',
