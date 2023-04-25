4626c8bc5e5d9679fb3d22e9930a3f48c973b012
Gökmen Görgen <gkmngrgn@gmail.com>
2023-04-26 01:46:19 +0300

    follow system theme on wezterm.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,31 @@
 local wezterm = require 'wezterm'
 
+function get_appearance()
+  if wezterm.gui then
+    return wezterm.gui.get_appearance()
+  end
+  return 'Dark'
+end
+
+function colors_for_appearance(appearance)
+  if appearance:find 'Dark' then
+    return {}
+  else
+    return {
+      foreground = 'black',
+      background = 'white',
+      ansi = { 'black', 'maroon', 'green', 'olive', 'navy', 'purple', 'teal', 'silver' },
+    }
+  end
+end
+
 return {
   font = wezterm.font {
     family = 'Jetbrains Mono',
     stretch = 'Expanded',
     weight = 'Regular',
   },
+  colors = colors_for_appearance(get_appearance()),
   font_size = 15,
   initial_cols = 160,
   initial_rows = 42,
@@ -15,4 +35,5 @@ return {
   audible_bell = 'Disabled',
   window_decorations = 'TITLE|RESIZE',
   default_cursor_style = 'BlinkingBlock',
+  bold_brightens_ansi_colors = false
 }
