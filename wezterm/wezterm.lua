559a13e2e04f92559525d16431a9dcf78ec86b53
Gökmen Görgen <gkmngrgn@gmail.com>
2023-06-10 17:38:58 +0300

    display specific settings.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,6 +1,6 @@
 local wezterm = require 'wezterm'
 
-function colors_for_appearance(appearance)
+function add_colors_for_appearance(settings, appearance)
   local fg, bg
 
   if appearance:find 'Dark' then
@@ -11,7 +11,7 @@ function colors_for_appearance(appearance)
     bg = 'white'
   end
 
-  return {
+  settings.colors = {
     foreground = fg,
     background = bg,
     tab_bar = {
@@ -21,23 +21,35 @@ function colors_for_appearance(appearance)
   }
 end
 
-return {
+function add_display_config(settings, display_name)
+  if display_name == 'internal' then
+    settings.font_size = 15.5
+    settings.window_padding = { top = 5, left = 0, right = 0, bottom = 0 }
+  elseif display_name == 'external' then
+    settings.font_size = 15
+    settings.line_height = 0.9
+    settings.window_padding = { top = 7, left = 0, right = 0, bottom = 0 }
+  end
+end
+
+wezterm_config = {
   font = wezterm.font {
     family = 'Jetbrains Mono',
     stretch = 'Expanded',
     weight = 'Regular',
   },
-  font_size = 17,
-  cell_width = 1.05,
-  colors = colors_for_appearance('Dark'),
   initial_cols = 160,
   initial_rows = 42,
   tab_max_width = 30,
   tab_bar_at_bottom = true,
   use_fancy_tab_bar = false,
-  window_padding = { top = 0, left = 0, right = 0, bottom = 0 },
   audible_bell = 'Disabled',
   window_decorations = 'TITLE|RESIZE',
   default_cursor_style = 'BlinkingBlock',
-  bold_brightens_ansi_colors = false
+  bold_brightens_ansi_colors = false,
 }
+
+add_colors_for_appearance(wezterm_config, 'Dark')
+add_display_config(wezterm_config, 'internal')
+
+return wezterm_config
