59de605936d61787ea2d45cd16b7673b554cbdae
Gökmen Görgen <gkmngrgn@gmail.com>
2024-01-01 23:48:36 +0100

    allow to rename tabs.

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,44 +1,66 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
+local config = wezterm.config_builder()
+local appearance = 'Dark'
 
-function add_colors_for_appearance(settings, appearance)
-    local fg, bg
-
-    if appearance:find 'Dark' then
-        fg = 'white'
-        bg = 'black'
-    else
-        fg = 'black'
-        bg = 'white'
-    end
-
-    settings.colors = {
-        foreground = fg,
-        background = bg,
-        tab_bar = {
-            background = bg,
-            active_tab = { bg_color = bg, fg_color = fg },
-        }
-    }
-end
-
-wezterm_config = {
-    font = wezterm.font {
-        family = 'Iosevka Term',
-        weight = 'Regular',
-    },
-    font_size = 16,
-    window_padding = { top = 0, left = 0, right = 0, bottom = 0 },
-    initial_cols = 160,
-    initial_rows = 42,
-    tab_max_width = 30,
-    tab_bar_at_bottom = true,
-    use_fancy_tab_bar = false,
-    audible_bell = 'Disabled',
-    window_decorations = 'TITLE|RESIZE',
-    default_cursor_style = 'BlinkingBlock',
-    bold_brightens_ansi_colors = false,
+-- KEYS
+config.keys = {
+   {
+      key = 'E',
+      mods = 'CTRL|SHIFT',
+      action = act.PromptInputLine {
+         description = 'RENAME TAB TITLE:',
+         action = wezterm.action_callback(
+            function(window, pane, line)
+               if line then
+                  window:active_tab():set_title(line)
+               end
+            end
+         ),
+      },
+   },
 }
 
-add_colors_for_appearance(wezterm_config, 'Dark')
+-- FONT
+config.font = wezterm.font {
+   family = 'Iosevka Term',
+   weight = 'Regular',
+}
+config.font_size = 16
 
-return wezterm_config
+-- WINDOW
+config.initial_cols = 160
+config.initial_rows = 42
+config.window_decorations = 'TITLE|RESIZE'
+config.window_padding = {
+   top = 0,
+   left = 0,
+   right = 0,
+   bottom = 0,
+}
+config.tab_max_width = 30
+config.tab_bar_at_bottom = true
+config.use_fancy_tab_bar = false
+config.audible_bell = 'Disabled'
+config.default_cursor_style = 'BlinkingBlock'
+config.bold_brightens_ansi_colors = false
+
+-- THEME
+if appearance:find 'Dark' then
+   fg = 'white'
+   bg = 'black'
+else
+   fg = 'black'
+   bg = 'white'
+end
+
+config.colors = {
+   foreground = fg,
+   background = bg,
+   tab_bar = {
+      background = bg,
+      active_tab = { bg_color = bg, fg_color = fg },
+   }
+}
+
+return config
