f37b220caa0545c06c935f0574cfc174af793f23
Gökmen Görgen <gkmngrgn@gmail.com>
2024-01-01 23:48:27 +0100

    add modular & mojo path.

--- home/.goedev-env.sh
+++ home/.goedev-env.sh
@@ -9,4 +9,7 @@ export LC_ALL="en_US.UTF-8"
 export PATH="$HOME/.local/bin:$PATH"
 export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
 
+export MODULAR_HOME="$HOME/.modular"
+export PATH="$MODULAR_HOME/pkg/packages.modular.com_mojo/bin:$PATH"
+
 test -f "$HOME/.cargo/env" && . "$HOME/.cargo/env"
